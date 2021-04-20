efc6838a1a61cf14e1ef06bb1e0bab5d7c9158de
Gene Bogdanovich <genebogdanovich@gmail.com>
2021-04-20 08:26:20 +0300

    Initial Commit

---
+++ ChildContextsForEditing/ChildContextsForEditingApp.swift
@@ -0,0 +1,20 @@
+//
+//  ChildContextsForEditingApp.swift
+//  ChildContextsForEditing
+//
+//  Created by Gene Bogdanovich on 20.04.21.
+//
+
+import SwiftUI
+
+@main
+struct ChildContextsForEditingApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
